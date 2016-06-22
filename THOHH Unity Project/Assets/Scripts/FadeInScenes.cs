2789015fb07c6958771e385b571006efaa85a0f4
Matthew Marshall <mamarshall44@gmail.com>
2016-06-22 03:46:26 -0700

    First Commit
    Made unity project, with art and simple splash screens

---
+++ THOHH Unity Project/Assets/Scripts/FadeInScenes.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeInScenes : MonoBehaviour {
+
+	public Image fadeIMG;	//Black Image in foreground
+	Color tempColor;		//used to Decrement the Alpha of image
+
+	// Use this for initialization
+	void Start () {
+		fadeIMG.gameObject.SetActive (true);
+		tempColor = fadeIMG.GetComponent<Image> ().color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fadeIMG.GetComponent<Image> ().color.a > 0f) 
+		{
+			tempColor.a -= Time.deltaTime;
+			fadeIMG.GetComponent<Image> ().color = tempColor;
+		}
+	}
+}
