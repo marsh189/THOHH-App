1a369c5972350ac7adf4dba058df54f03fd101a3
Matthew Marshall <mamarshall44@gmail.com>
2016-06-24 14:48:13 -0700

    Added page images

---
+++ THOHH Unity Project/Assets/Scripts/ChangePages.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangePages : MonoBehaviour {
+
+	private Touch initialTouch = new Touch ();
+	private float distance = 0f;
+	private bool swiped = false;
+
+	void FixedUpdate () 
+	{
+		foreach (Touch t in Input.touches) 
+		{
+			if (t.phase == TouchPhase.Began) 
+			{
+				initialTouch = t;
+			} 
+			else if (t.phase == TouchPhase.Moved && !swiped) 
+			{
+				//distance formula
+				float deltaX = initialTouch.position.x - t.position.x;
+				float deltaY = initialTouch.position.y - t.position.y;
+				distance = Mathf.Sqrt ((deltaX * deltaX) + (deltaY * deltaY));
+				//direction
+				if (distance > 100f) 
+				{
+					if (deltaX > 0) //swiped left
+					{
+						this.gameObject.SetActive (false);
+						//BackPage ();
+					} 
+					else if(deltaX <= 0) //swiped right
+					{
+						this.gameObject.SetActive (true);
+						//NextPage ();
+					}
+				}
+
+				swiped = true;
+			}
+			else if (t.phase == TouchPhase.Ended) 
+			{
+				initialTouch = new Touch ();
+				swiped = false;
+			}
+		}
+	}
+}
